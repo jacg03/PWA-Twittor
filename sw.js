0f5b0109810473a59542cf2a6cb57c01266b302d
Jesus Caballero <jesus.caballero@neoris.com>
2019-07-17 11:46:41 -0500

    Adding functionality to remove old cache versions

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 importScripts('js/sw-utils.js');
 
 // CACHE NAMES
-const STATIC_CACHE = 'static-v3';
-const DYNAMIC_CACHE = 'dynamic-v1';
+const STATIC_CACHE = 'static-v4';
+const DYNAMIC_CACHE = 'dynamic-v2';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
 // APPLICATION HEART
@@ -44,10 +44,13 @@ self.addEventListener('install', e => {
 });
 
 self.addEventListener('activate', e => {
-    const response = caches.open().keys()
+    const response = caches.keys()
         .then(keys => {
             keys.forEach(key => {
                 if (key !== STATIC_CACHE && key.includes('static')) {
+                    return caches.delete(key);
+                }
+                if (key !== DYNAMIC_CACHE && key.includes('dynamic')) {
                     return caches.delete(key);
                 }
             });
